eda2c972131cb215f73b0fa9bdbdcc323c23c1c3
Teo <teo@journeyxp.com>
2016-11-15 15:31:36 +0100

    Minor pom changes.

--- src/main/java/dk/cphbusiness/choir/frontend/ChoirManagerController.java
+++ src/main/java/dk/cphbusiness/choir/frontend/ChoirManagerController.java
@@ -1,24 +1,30 @@
 package dk.cphbusiness.choir.frontend;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Collection;
+
+import server.*;
 
 @WebServlet(name = "ChoirManagerController", urlPatterns = {"/ChoirManager"})
 public class ChoirManagerController extends HttpServlet {
   
 //  private ChoirManager manager = new ChoirManagerDummy();
 
-//  @Override
-//  protected void service(
-//      HttpServletRequest request,
-//      HttpServletResponse response
-//      ) throws ServletException, IOException {
+  @Override
+  protected void service(
+      HttpServletRequest request,
+      HttpServletResponse response
+      ) throws ServletException, IOException {
+      
+      BackendMockHttpServer backendMock = new BackendMockHttpServer();
+      
+      backendMock.run();
+      
 //    PrintWriter out = response.getWriter();
 //    try {
 ////      Collection<MemberSummary> members = manager.listMembers();
@@ -31,6 +37,6 @@ public class ChoirManagerController extends HttpServlet {
 //      e.printStackTrace(out);
 //      out.println("</pre>");
 //      }
-//    }
+    }
   
   }
