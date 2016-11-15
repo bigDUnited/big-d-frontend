38160b7acc5a822d7ff75abc016d3a9f23ad259f
theIncredibleMarek <marek.furak+github@gmail.com>
2016-11-15 15:19:25 +0100

    Added better styling for summary page, improved html and js

--- src/main/webapp/js/summary.js
+++ src/main/webapp/js/summary.js
@@ -1,29 +1,22 @@
-$(document).ready(function() {
-	var url = window.location.pathname
-	console.log("shit");
-	console.log(url);
-	var reservationid = url.substring(url.lastIndexOf("/") + 1, url.length);
-	console.log(reservationId);
-	var serverUrl = "boyko's server" + reservationId;
-	$.get(URL)
-	    .done(function(data) {
-	    	var tempText = $("#reservationId").text();
-	    	$("#reservationId").text(tempText + last);
-	 		tempText = $("#location").text();
-	    	$("#location").text(tempText + data.location);
-	    	var tempText = $("#route").text();
-	    	$("#route").text(tempText + data.route);
-	    	var tempText = $("#journey").text();
-	    	$("#journey").text(tempText + data.journey);
-	    	
-	    	   	
-	 
-	    })
-	    .fail(function() {
-	    	$("#reservationId").text("Invalid reservation id.");
-			$("#location").hide();
-			$("#route").hide();
-			$("#journey").hide();
-	    })
-	
-})
+$(document).ready(function () {
+    var url = window.location.pathname;
+    console.log("shit");
+    console.log(url);
+    var reservationId = url.substring(url.lastIndexOf("/") + 1, url.length);
+    console.log(reservationId);
+    var serverUrl = "boyko's server" + reservationId;
+    $.get(URL)
+            .done(function (data) {
+                var tempText = $("#reservationId").text();
+                $("#reservationId").text(tempText + last);
+                tempText = $("#location").text();
+                $("#location").text(tempText + data.location);
+                tempText = $("#route").text();
+                $("#route").text(tempText + data.route);
+                tempText = $("#journey").text();
+                $("#journey").text(tempText + data.journey);
+            })
+            .fail(function () {
+                $("#reservationId").text("Invalid reservation id.");
+            });
+});
